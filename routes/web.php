97aa5bf8a5dee9ee75ba92d3d0ad1ff7b3d1d2d1
Ross260 <rossketcha@gmail.com>
2025-01-31 08:06:20 +0100

    route configuration

--- routes/web.php
+++ routes/web.php
@@ -18,27 +18,30 @@ Route::get('/', function () { // lorsque j'accede à la page racine, répond par
     return view('welcome');
 });
 
+Route::prefix('/blog')->name('blog.')->group(function () {  //permet de grouper les routes qui ont un meme préfixe, 
+    // mettre toutes ces routes à l'interieur en enlevant le préfixe vu qu'il est déja marqué ici
+    Route::get('/', function (Request $request) {
+        return [
 
-Route::get('/blog', function (Request $request) {
-    return [
+            "name" => $request->input('name','Doe'), // retourne le nom, s'il n'y a pas de nom en entrer renvoie la valeur par défaut
+            "Article" => "article 1",
+            "link" => route('blog.show', ['slug' => 'article', 'id' => 12 ]),
+        ];
+    })->name('index');
 
-        "name" => $request->input('name','Doe'), // retourne le nom, s'il n'y a pas de nom en entrer renvoie la valeur par défaut
-        "Article" => "article 1",
-        "link" => route('blog.show', ['slug' => 'article', 'id' => 12 ]),
-    ];
-})->name('blog.index');
+    Route::get( '/{slug}-{id}', function (string $slug, string $id) { 
+        // les {} permettes de dire qu'on aura un parametre dynamique
+        // il est tout à fait possible d'ajouter le $request comme 3e parametre et de l'utiliser
 
-Route::get( '/blog/{slug}-{id}', function (string $slug, string $id) { 
-    // les {} permettes de dire qu'on aura un parametre dynamique
-    // il est tout à fait possible d'ajouter le $request comme 3e parametre et de l'utiliser
+        return [
+            "slug" => $slug,
+            "id" => $id
+        ];
+        // on peut ajouter des methodes suplémentaires sur des routes
+        // where() :: est une methode permettant de spécifier des contrainte sur un paramètre
+    })->where([
+        'id' => '[0-9]+', // the + to specify that they are only a numerical values
+        'slug' => '[a-z0-9\-]+' // to specify that we could have characters between a and z, 0 and 9 and also -. and the + to specify thay it could be repeated more than once
+    ])->name('show');
+});
 
-    return [
-        "slug" => $slug,
-        "id" => $id
-    ];
-    // on peut ajouter des methodes suplémentaires sur des routes
-    // where() :: est une methode permettant de spécifier des contrainte sur un paramètre
-})->where([
-    'id' => '[0-9]+', // the + to specify that they are only a numerical values
-    'slug' => '[a-z0-9\-]+' // to specify that we could have characters between a and z, 0 and 9 and also -. and the + to specify thay it could be repeated more than once
-])->name('blog.show');
